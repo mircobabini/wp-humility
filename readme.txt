55c9c94d1dcf536ec54aaa17321081bd2ccb9555
Mirco Babini <mirkolofio@gmail.com>
2013-12-23 22:28:32 +0100

    1.0.0 - Mailchimp for Wordpress, CodeCanyon LayerSlider

---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Humility ===
+Contributors: mirkolofio
+Donate link: http://mircobabini.com/donate/
+Tags: font, style, theme, love
+Requires at least: 2.8
+Tested up to: 3.8
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Make a bunch of exuberant plugins humble
+
+== Description ==
+
+This humilize a bunch of exuberant plugins like Mailchimp for Wordpress, CodeCanyon LayerSlider and more.
+
+Developed by [Mirco Babini](http://www.mircobabini.com/), **Wordpress Consultant, Web Developer and Data Lover**.
+
+== Installation ==
+
+1. Upload `wp-humility/` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+== Changelog ==
+
+= 1.0.0 =
+* Mailchimp for Wordpress
+* CodeCanyon LayerSlider
+
---
+++ wp-humility.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Plugin Name: Humility
+Plugin URI: http://github.com/mirkolofio/wp-humility
+Description: Make a bunch of exuberant plugins humble
+Author: Mirco Babini
+Version: 1.0.0
+Author URI: http://github.com/mirkolofio
+*/
+
+if ( is_admin() ) { // make 'em humble
+
+	wp_register_style ('humility', plugin_dir_url (__FILE__) . 'style.css');
+	wp_enqueue_style ('humility');
+	
+	add_action( 'plugins_loaded', function() {
+	
+		// humble! remove wpmu plugins' alert when no 'Update Notifications' plugin detected; https://premium.wpmudev.org/projects/
+		remove_action( 'admin_notices', 'wdp_un_check', 5 );
+		remove_action( 'network_admin_notices', 'wdp_un_check', 5 );
+
+	} );
+	
+	add_action( 'admin_menu', function() {
+
+		global $menu;
+		global $submenu;
+
+		foreach ($menu as $key => &$menu_item) {
+			switch ($menu_item[0]) {
+
+				// humble! Shortens menu item for 'MailChimp for WordPress' plugin; http://bit.ly/mailchimp-for-wordpress
+				case 'MailChimp for WP':
+					$menu_item[0]  = 'MailChimp';
+					$menu_item[0] .= '<br/><p class="humble-subtitle">MailChimp for WP';
+					$menu_item[4] .= ' humble-menu-large';
+					break;
+
+				// humble! Shortens menu item for 'LayerSlider Responsive WordPress Slider' plugin; http://bit.ly/codecanyon-layerslider
+				case 'LayerSlider WP':
+					$menu_item[0] = 'LayerSlider';
+					break;
+			}
+		}
+
+	} );
+
+}
